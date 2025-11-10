0ab19b0d9e5d3fb4c360b6bda10519af64d03bdb
beca-droid <rndancarina@gmail.com>
2025-11-10 09:50:32 -0300

    Primeiro git

---
+++ assets/script.js
@@ -0,0 +1,7 @@
+const resposta = document.getElementById("feedback")
+const btn = document.getElementById("btn")
+
+btn.addEventListener("submit", function() {
+
+  window.alert("Você foi ouvido, muito obrigada pelo comentário!")
+})
