0e9ea44e24584ec5a75a15e4b92009ff00b6923c
beca-droid <rndancarina@gmail.com>
2025-11-11 14:46:22 -0300

    Primeiro git

--- assets/script.js
+++ assets/script.js
@@ -5,3 +5,21 @@ btn.addEventListener("click", function() {
 
   window.alert("Você foi ouvido, muito obrigada pelo comentário!")
 })
+
+const fundo = [
+  "lightblue",
+  "red",
+  "plum",
+  "lightyellow",
+  "lightgray"
+]
+
+const cor =  document.getElementById("cor")
+
+cor.addEventListener("click", function() {
+
+  const corAleatoria = fundo[Math.floor(Math.random() * fundo.length)]
+  console.log(corAleatoria)
+
+  document.body.style.backgroundColor = corAleatoria
+})
